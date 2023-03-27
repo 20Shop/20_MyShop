1f69667aa47878519dbfd1403b6b51b84b174689
20Shop <129061236+20Shop@users.noreply.github.com>
2023-03-27 18:26:36 +0700

    Add files via upload

---
+++ h.php
@@ -0,0 +1,8 @@
+<?php 
+session_start();
+?>    
+    
+    <title> 20_SHOP │ ขายสินค้าราคาถูกทุกอย่าง 20 บาท </title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link  href="css/style.css" rel="stylesheet" />
---
+++ logout3.php
@@ -0,0 +1,6 @@
+<?php
+echo "<script>";
+			echo "alert('กรุณาลงชื่อเข้าใช้ หรือสมัครสมาชิกก่อนสั่งซื้อนะครับ !');";
+			echo "window.location ='login.php'; ";
+echo "</script>";
+?>
